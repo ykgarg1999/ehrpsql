e39611738afddfc55a98f7ccc1151a0bea25bc1b
Yeshu Garg <ykgarg1999@gmail.com>
2022-01-23 21:42:06 +0530

    finally

--- healthcaresystem/settings.py
+++ healthcaresystem/settings.py
@@ -187,13 +187,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = 'serviceonlineehr@gmail.com'
-EMAIL_HOST_PASSWORD = 'kitelredgfkuynsv'
+EMAIL_HOST_USER = 'adityaonlineehr@gmail.com'
+EMAIL_HOST_PASSWORD = 'admin1234!'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 AUTH_USER_MODEL = 'accounts.User'
 
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
+    '*'
 ]
